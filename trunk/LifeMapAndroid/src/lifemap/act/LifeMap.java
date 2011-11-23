825439f2acccb2182073af2a05247a0da632a981
phucF8@gmail.com <phucF8@gmail.com@81ae4217-e253-f3f7-48e4-8d086e115675>
2011-11-23 02:21:35 +0000

    Initial import.

---
+++ trunk/LifeMapAndroid/src/lifemap/act/LifeMap.java
@@ -0,0 +1,13 @@
+package lifemap.act;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LifeMap extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
